6b3d28cceaac8d9c98e7e76009d2400cd76cd7ad
Dmitrii <dmitrii1997033@gmail.com>
2024-05-13 12:25:21 +0300

    adding hover to the menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,31 +11,22 @@ const Nav = () => {
   return (
     <aside className="bg-[#393C49]">
       <nav className="bg-[#1F1D2B] rounded-r-2xl">
-        <ul className="">
-          <li className="">
-            <NavLink
-              to="/some"
-              className={({ isActive }) =>
-                isActive
-                  ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
-              }
-            >
-              <Home className="p-3" />
+        <ul className="flex flex-col gap-y-5 min-h-[100vh] min-w-[5vw] [&>*:last-child]:mt-auto [&>*:first-child]:mb-5">
+          <li className="group">
+            <NavLink to='/home' className='fill-current text-[#EA7C69] flex justify-center p-3 ml-2'>
+              <Store className='p-3 bg-[rgba(235,150,106,0.26)] rounded-xl '/>
             </NavLink>
           </li>
-          <li className="">
+          <li className="group">
             <NavLink
               to="/some"
               className={({ isActive }) =>
                 isActive
                   ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2  hover:curve_hover hover:text-white "
               }
             >
-              {({ isActive }) => (
-                <Store className={isActive ? "" : "p-3 fill-current "} />
-              )}
+              <Home className="p-3 group-hover:rounded-xl group-hover:bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]" />
             </NavLink>
           </li>
           <li className="">
@@ -53,62 +44,74 @@ after:shadow-[10px_10px_0_rgb(57,60,73)]`
               }
             >
               {({ isActive }) => (
-                <Statistics
+                <Discounts
                   className={
                     isActive
-                      ? "fill-current text-white p-3 rounded-xl bg-[#EA7C69]"
+                      ? "fill-current text-white p-3 rounded-xl bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]"
                       : "p-3 fill-current "
                   }
                 />
               )}
             </NavLink>
           </li>
-          <li className="">
+          <li className="group">
             <NavLink
               to="/some"
               className={({ isActive }) =>
                 isActive
                   ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2  hover:curve_hover hover:text-white "
               }
             >
-              <Messages className="p-3" />
+              <Statistics className="p-3 group-hover:rounded-xl group-hover:bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]" />
             </NavLink>
           </li>
-          <li className="">
+          <li className="group">
             <NavLink
               to="/some"
               className={({ isActive }) =>
                 isActive
                   ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2  hover:curve_hover hover:text-white "
               }
             >
-              <Notifications className="p-3" />
+              <Messages className="p-3 group-hover:rounded-xl group-hover:bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]" />
             </NavLink>
           </li>
-          <li className="">
+          <li className="group">
             <NavLink
               to="/some"
               className={({ isActive }) =>
                 isActive
                   ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2  hover:curve_hover hover:text-white "
               }
             >
-              <Notifications className="p-3" />
+              <Notifications className="p-3 group-hover:rounded-xl group-hover:bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]" />
             </NavLink>
           </li>
-          <li className="">
+          <li className="group">
             <NavLink
               to="/some"
               className={({ isActive }) =>
                 isActive
                   ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
-                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2  hover:curve_hover hover:text-white "
               }
             >
-              <Notifications className="p-3" />
+              <Settings className="p-3 group-hover:rounded-xl group-hover:bg-[#EA7C69] group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)]" />
+            </NavLink>
+          </li>
+          <li className="group">
+            <NavLink
+              to="/some"
+              className={({ isActive }) =>
+                isActive
+                  ? "flex justify-center p-3 ml-2 rounded-l-xl bg-[#393C49] relative before:absolute before:w-5  before:h-5 before:right-0 before:-bottom-5 before:bg-[#1F1D2B] before:rounded-full before:shadow-[10px_-10px_0_rgb(57,60,73)] after:absolute after:w-5  after:h-5 after:right-0 after:-top-5 after:bg-[#1F1D2B] after:rounded-full after:shadow-[10px_10px_0_rgb(57,60,73)]"
+                  : "fill-current text-[#EA7C69] flex justify-center p-3 ml-2 hover:curve_hover_bottom hover:text-white "
+              }
+            >
+              <LogOut className="p-3 group-hover:bg-[#EA7C69] group-hover:rounded-xl group-hover:shadow-[0_4px_11px_0_rgba(234,124,105,0.32)] mb-5" />
             </NavLink>
           </li>
         </ul>
--- src/pages/layouts/Public.tsx
+++ src/pages/layouts/Public.tsx
@@ -1,5 +1,5 @@
 const Public = () => {
-  return <div className="bg-[#393C49] grow">Public</div>;
+  return <div className="bg-[#393C49] grow min-h-[100vh] text-white">Public</div>;
 };
 
 export default Public;
